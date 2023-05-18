da3cbdd6cee66164d7c8e3f1117c80615b019655
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-05-18 00:48:22 -0300

    Ajustes na porta do servidor

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
     console.log('Servidor rodando na porta', port);
