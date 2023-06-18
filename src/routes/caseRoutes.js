b05deeea3b07bb11f92008358e73d9104caf6700
YagoASantos <yagoaguiar9@gmail.com>
2023-06-18 13:18:41 -0300

    Inserindo a função de listar casos por status.

--- src/routes/caseRoutes.js
+++ src/routes/caseRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/cases', authMiddleware.verifyToken, caseController.getAll);
-router.post('/cases/status', authMiddleware.verifyToken, caseMiddleware.VerifyStatusValue,
+router.get('/cases/status', authMiddleware.verifyToken, caseMiddleware.VerifyStatusValue,
     caseController.catchCaseByStatus);
 router.post('/cases', authMiddleware.verifyToken, caseMiddleware.ValidatorBody, caseController.create);
 router.put('/cases/:id', authMiddleware.verifyToken, caseMiddleware.ValidatorBody, caseController.update);
